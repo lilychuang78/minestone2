d44ca49dc2768ffaa2e9d78bfc0b505605428a29
lilychuang78 <lilychuang78@gmail.com>
2020-09-23 16:45:24 +0000

    add google local map api

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,15 @@
-const localContextMapView = new google.maps.localContext.LocalContextMapView({
-  element: document.getElementById('map'),
-  placeTypePreferences: ['restaurant', 'tourist_attraction'],
-  maxPlaceCount: 12,
-});
+let map;
+    function initMap() {
+      const localContextMapView = new google.maps.localContext.LocalContextMapView({
+        element: document.getElementById('map'),
+        placeTypePreferences: ['restaurant', 'tourist_attraction'],
+        maxPlaceCount: 12,
+      });
+
+      map = localContextMapView.map;
+
+      map.setOptions({
+        center: {lat: 24, lng: 120},
+        zoom: 7
+      });
+    }
