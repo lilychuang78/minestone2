c64026fb93fedb8641d99bbf9184361adf5352b0
lilychuang78 <lilychuang78@gmail.com>
2020-09-24 13:45:01 +0000

    update service id and template id in emailjs

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,12 +1,8 @@
 function sendMail(contactForm) {
-    // Just for test
-    console.log(contactForm.emailaddress.value);
-    console.log(contactForm.projectsummary.value);
-    // 
-    emailjs.send("service_mb8k0vn", "template_8mr44vr", {
-        "from_name": contactForm.name.value, //name is given in contact.html
-        "from_email": contactForm.emailaddress.value, //emailaddress is given in contact.html
-        "project_request": contactForm.projectsummary.value //projectsummary is given in contact.html
+    emailjs.send("service_4vbdhwr", "template_pxecv3f", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.summary.value
     }, "user_SymNMEJn92ZzIBFoCXRIY")
     .then(
         function(response) {
