a2cd162f35e9425e97f611e52015d49e2d38aa58
lilychuang78 <lilychuang78@gmail.com>
2020-09-25 11:28:57 +0000

    beutify code using https://www.freeformatter.com/

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,17 +1,17 @@
 function sendMail(contactForm) {
-    emailjs.send("service_4vbdhwr", "template_pxecv3f", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.summary.value
-    }, "user_SymNMEJn92ZzIBFoCXRIY")
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    contactForm.reset();
-    return false;  // To block from loading a new page
+	emailjs.send("service_4vbdhwr", "template_pxecv3f", {
+			"from_name": contactForm.name.value,
+			"from_email": contactForm.emailaddress.value,
+			"project_request": contactForm.summary.value
+		}, "user_SymNMEJn92ZzIBFoCXRIY")
+		.then(
+			function (response) {
+				console.log("SUCCESS", response);
+			},
+			function (error) {
+				console.log("FAILED", error);
+			}
+		);
+	contactForm.reset();
+	return false; // To block from loading a new page
 }
--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,23 @@
 let map;
-    function initMap() {
+    function initMap() {let map;
+
+function initMap() {
+	const localContextMapView = new google.maps.localContext.LocalContextMapView({
+		element: document.getElementById('map'),
+		placeTypePreferences: ['restaurant', 'tourist_attraction', 'bakery', 'book_store', 'cafe', 'night_club', 'park', 'shopping_mall', 'bar'],
+		maxPlaceCount: 10,
+	});
+
+	map = localContextMapView.map;
+
+	map.setOptions({
+		center: {
+			lat: 25.033964,
+			lng: 121.564468
+		},
+		zoom: 13
+	});
+}
       const localContextMapView = new google.maps.localContext.LocalContextMapView({
         element: document.getElementById('map'),
         placeTypePreferences: ['restaurant', 'tourist_attraction', 'bakery', 'book_store', 'cafe', 'night_club', 'park', 'shopping_mall', 'bar'],
