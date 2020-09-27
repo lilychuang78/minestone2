a8d98aadad2fba4fd40a98a2b321fb9a7cc501ce
lilychuang78 <67637364+lilychuang78@users.noreply.github.com>
2020-09-27 17:39:54 +0200

    Update email.js
    change placeholder text using jquery (resource: https://stackoverflow.com/questions/9232810/change-placeholder-text-using-jquery)

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,7 +2,7 @@ function sendMail(contactForm) {
 	emailjs.send("service_4vbdhwr", "template_pxecv3f", {
 			"from_name": contactForm.name.value,
 			"from_email": contactForm.emailaddress.value,
-			"project_request": contactForm.summary.value
+			"message": contactForm.summary.value
 		}, "user_SymNMEJn92ZzIBFoCXRIY")
 		.then(
 			function (response) {
@@ -15,3 +15,14 @@ function sendMail(contactForm) {
 	contactForm.reset();
 	return false; // To block from loading a new page
 }
+
+//https://stackoverflow.com/questions/9232810/change-placeholder-text-using-jquery//
+$(document).ready(function(){ 
+  $('form').find("textarea").each(function(ev)
+  {
+      if(!$(this).val()) { 
+     $(this).attr("placeholder", "Your message is received!");
+  }
+  });
+});
+//
