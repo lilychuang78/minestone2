03f6b45aeaaf3a2c028e12ef6d087f04a9768e63
lilychuang78 <lilychuang78@gmail.com>
2020-11-23 20:17:40 +0000

    removed duplicated lines in map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,34 +1,15 @@
 let map;
-    function initMap() {let map;
-
 function initMap() {
-	const localContextMapView = new google.maps.localContext.LocalContextMapView({
-		element: document.getElementById('map'),
-		placeTypePreferences: ['restaurant', 'tourist_attraction', 'bakery', 'book_store', 'cafe', 'night_club', 'park', 'shopping_mall', 'bar'],
-		maxPlaceCount: 10,
-	});
-
-	map = localContextMapView.map;
-
-	map.setOptions({
-		center: {
-			lat: 25.033964,
-			lng: 121.564468
-		},
-		zoom: 13
-	});
-}
-      const localContextMapView = new google.maps.localContext.LocalContextMapView({
+    const localContextMapView = new google.maps.localContext.LocalContextMapView({
         element: document.getElementById('map'),
         placeTypePreferences: ['restaurant', 'tourist_attraction', 'bakery', 'book_store', 'cafe', 'night_club', 'park', 'shopping_mall', 'bar'],
         maxPlaceCount: 10,
-      });
+    });
 
-      map = localContextMapView.map;
+    map = localContextMapView.map;
 
-      map.setOptions({
-        center: {lat: 25.033964, lng: 121.564468},
+    map.setOptions({
+        center: { lat: 25.033964, lng: 121.564468 },
         zoom: 13
-      });
-    }
-    
+    });
+}
